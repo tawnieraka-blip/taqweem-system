b3422b12ce7affaa4493bb659f0bc93412c77f02
tawnieraka-blip <tawnie.raka@gmail.com>
2026-07-19 06:56:33 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -9,9 +9,7 @@ const CONFIG = {
 
     VERSION: "1.0.0",
 
-    const CONFIG = {
-
-    API_URL: "https://script.google.com/macros/s/AKfycbziZNncJOGQ85AFcM52f34-oYjcpTsvz7O0ojhf733Xvi1ayzjZR-e7QoPye5J59UxK/exec"
+    API_URL: "https://script.google.com/macros/s/AKfycbziZNncJOGQ85AFcM52f34-oYjcpTsvz7O0ojhf733Xvi1ayzjZR-e7QoPye5J59UxK/exec",
 
     DEBUG: true
 
